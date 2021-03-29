17c53800312eb4281e53ec2d829f3cf4d939d702
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-03-29 16:33:08 -0400

    dashboard login and register function works

--- accounts/views.py
+++ accounts/views.py
@@ -8,8 +8,19 @@ def home(request):
 	return render (request , 'accounts/home.html' )
 
 def login (request):
-	if request.medhod == 'POST':
+	if request.method == 'POST':
 		username = request.POST['username']
+		password = request.POST['password']
+
+		user = auth.authenticate(username=username,password=password)
+
+		if user is not None:
+			auth.login(request,user)
+			messages.success(request , 'You are now logged in.!')
+			return redirect('dashboard')
+		else:
+			messages.error(request, 'Invalid login credentials')
+			return redirect('login')
 
 	return render (request , 'accounts/login.html' )
 
@@ -51,4 +62,12 @@ def dashboard (request):
 	return render (request , 'accounts/dashboard.html' )
 
 def logout (request):
+	if request.method == 'POST':
+		auth.logout(request)
+		messages.success(request,'You are successfully logout')
+		return redirect('home')
 	return redirect ('home')
+
+
+
+
